4bb50bfa528d816365f06c5a7a658011c943028a
seitamuro <seitamuro0210@gmail.com>
2024-04-10 00:09:04 +0900

    add date function & api

---
+++ date/app.ts
@@ -0,0 +1,34 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import date from "date-and-time";
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  try {
+    const now = new Date();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: date.format(now, "YYYY/MM/DD HH:mm:ss"),
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "some error happened",
+      }),
+    };
+  }
+};
